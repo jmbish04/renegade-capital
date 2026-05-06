20d58b90970c2ea63812d82b147fc0e8e90340dd
jmbish04 <jmbish04@gmail.com>
2026-05-06 00:11:16 -0700

    Set srcDir to './src/frontend' in config

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  srcDir: './src/frontend',
   output: 'server',
   adapter: cloudflare(),
   vite: {
